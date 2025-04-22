3cff71bfd2e5d73d51cfa8cc9d83a22e17a8de90
ImFiru <almanajrafiras2004@gmail.com>
2025-04-22 13:38:46 +0000

    feat: original template

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+import path from "path";
+
+export default defineConfig(({ mode }) => {
+    const isProduction = mode === "production";
+
+    return {
+        css: {
+            postcss: {
+                plugins: [
+                    autoprefixer(),
+                    isProduction && cssnano(),
+                ].filter(Boolean),
+            },
+        },
+        build: {
+            outDir: "static",
+            emptyOutDir: false,
+            rollupOptions: {
+                input: path.resolve(__dirname, "static/assets/scss/custom.scss"),
+                output: {
+                    assetFileNames: (assetInfo) => {
+                        if (assetInfo.name === "custom.css") {
+                            return "assets/css/custom.css";
+                        }
+                        return "assets/css/[name].[ext]";
+                    },
+                },
+            },
+        },
+        server: {
+            cors: true,
+            host: '0.0.0.0',
+            hmr: {
+                host: '192.168.1.147'
+            },
+        },
+    };
+});
