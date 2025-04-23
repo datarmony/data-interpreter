aca87cc3c6b3a76fa72e181a734b55f4d0342fd6
ImFiru <almanajrafiras2004@gmail.com>
2025-04-23 09:31:04 +0000

    feat: added create dashboard button and fixed theme swicher

--- static/assets/js/layoutTheme.js
+++ static/assets/js/layoutTheme.js
@@ -18,13 +18,18 @@ document.getElementById("p_dark_mode").addEventListener("change", function (even
 })
 
 
+// layoutTheme.js
+
 function layout_change(layout) {
     var control = document.querySelector('#p_dark_mode');
     var icon = document.querySelector('#p_mode_icon');
+    var logo = document.querySelector('#header-logo'); // Selecciona el logo por ID
 
+    // Establece el atributo del tema en el body
     document.getElementsByTagName('body')[0].setAttribute('data-pc-theme', layout);
 
     if (layout === 'dark') {
+        // --- Modo Oscuro ---
         dark_flag = true;
         localStorage.setItem('dark_layout', 'true');
 
@@ -32,18 +37,29 @@ function layout_change(layout) {
             control.checked = true;
         }
         if (icon) {
-            icon.classList.replace("ti-sun-off", "ti-sun")
+            icon.classList.remove("ti-sun-off");
+            icon.classList.add("ti-sun");
+        }
+        if (logo && logo.dataset.darkSrc) {
+            logo.src = logo.dataset.darkSrc;
         }
 
     } else {
+        // --- Modo Claro ---
         dark_flag = false;
         localStorage.setItem('dark_layout', 'false');
 
         if (control) {
             control.checked = false;
         }
+
         if (icon) {
-            icon.classList.replace("ti-sun", "ti-sun-off")
+            icon.classList.remove("ti-sun");
+            icon.classList.add("ti-sun-off");
+        }
+
+        if (logo && logo.dataset.lightSrc) {
+            logo.src = logo.dataset.lightSrc;
         }
     }
 }
